e1bf602c8727bc279c9b1553dc704ccd22508343
Valtof01 <v.moureau@net.estia.fr>
2024-04-16 17:24:40 +0200

    page login

--- ajax.js
+++ ajax.js
@@ -1,16 +1,3 @@
-function loadData() {
-    // Requête AJAX pour récupérer les données du serveur
-    $.ajax({
-        url: 'donnees.php',
-        success: function(data) {
-            // Mise à jour de la page web avec les nouvelles données
-            $('#contenu').html(data);
-        }
-    });
-}
-
-// Appel initial de la fonction loadData()
-loadData();
-
-// Mise à jour de la page web toutes les 5 secondes
-setInterval(loadData, 50);
+var reloadInterval = setInterval(function() {
+    location.reload();
+}, 50);

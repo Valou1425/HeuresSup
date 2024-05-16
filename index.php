5c596dc43981046dce4f010e2e18642c3bffce53
Hugo <h.boue@net.estia.fr>
2024-05-16 11:38:38 +0200

    pull de hugo

--- index.php
+++ index.php
@@ -4,21 +4,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="Vue/Styles/style.css"> 
-        <link rel="stylesheet" href="Vue/styles/desktop.css" media="screen and (min-width: 768px)"> 
+        <link rel="stylesheet" href="Vue/Styles/style.css">
+        <link rel="stylesheet" href="Vue/styles/desktop.css">
         <link rel="stylesheet" href="styles/mobile.css" media="screen and (max-width: 767px)"> 
         <link rel="shortcut icon" href="media/pictures/favicon.png">
         <title>Document</title>
     </head>
     <body>
-        <?php
-
-        if (isset($_GET['connect'])) {
-            echo('<p id="notification">identifiant ou mot de passe incorect</p>');
-        }
-        ?>
+        
         <main class="center">
-            <form action="./Controller/loginController.php" method="post">
+            <form action="./Controller/TestController.php" method="post">
                 <fieldset>
                     <h2>Connexion</h2>
                     <p>Entrez vos informations ici</p>
@@ -33,10 +28,9 @@
                         </div>
                         <input id="password" name="password" type="password">
                     </p>
-                    <input id="vert" type="submit" value="connexion"> 
+                    <input id="vert" type="submit" onclick="window.location.href='test.php';" value="connexion"> 
                 </fieldset>
             </form>
-
         </main>
     </body>
 </html>

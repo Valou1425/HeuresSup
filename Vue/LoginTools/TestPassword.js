ed76f397140054d65890e072ff172811867ff29e
Hugo <h.boue@net.estia.fr>
2024-05-16 10:59:45 +0200

    Change Password

---
+++ Vue/LoginTools/TestPassword.js
@@ -0,0 +1,27 @@
+function validatePassword() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    const passwordError = document.getElementById('password-error');
+    const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$/;
+
+    if (!regex.test(password)) {
+        passwordError.textContent = '⚠ Mot de passe incomplet';
+        passwordError.style.display = 'block';
+    } else {
+        passwordError.style.display = 'none';
+    }
+
+    if (password !== confirmPassword) {
+        passwordError.textContent = '⚠ Le Mot de passe doit contenir un caractère spécial, un chiffre, et une majuscule.';
+        passwordError.style.display = 'block';
+    }
+}
+
+document.getElementById('password-form').onsubmit = function (event) {
+    validatePassword();
+    const isVisible = document.getElementById('password-error').style.display === 'block';
+    if (isVisible) {
+        event.preventDefault();
+    }
+}
+
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="Vue/Styles/style.css"> 
-        <link rel="stylesheet" href="Vue/styles/desktop.css" media="screen and (min-width: 768px)"> 
+        <link rel="stylesheet" href="Vue/styles/desktop.css"> 
         <link rel="stylesheet" href="styles/mobile.css" media="screen and (max-width: 767px)"> 
         <link rel="shortcut icon" href="media/pictures/favicon.png">
         <title>Document</title>
@@ -24,14 +24,13 @@
                     <p class="colonne">
                         <div class="ligne">
                             <label for="password" class="bold">mot de passe</label> 
-                            <a href="./en-cours.html" ><span id="SpanForm1">mot de passe oublié?</span></a>
+                            <a href="./Vue/LoginTools/ChangerMotDePasse.php" ><span id="SpanForm1">mot de passe oublié?</span></a>
                         </div>
                         <input id="password" name="password" type="password">
                     </p>
-                    <input id="vert" type="submit" value="connexion"> 
+                    <input id="vert" type="submit" onclick="window.location.href='test.php';" value="connexion"> 
                 </fieldset>
             </form>
-
         </main>
     </body>
 </html>
